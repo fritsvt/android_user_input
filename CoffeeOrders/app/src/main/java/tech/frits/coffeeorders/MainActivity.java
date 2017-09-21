d6657465f987c35fc30bc908da316401f87160cc
Frits <frits.vanturnhout@gmail.com>
2017-09-21 13:32:33 +0200

    coffee app done

--- CoffeeOrders/app/src/main/java/tech/frits/coffeeorders/MainActivity.java
+++ CoffeeOrders/app/src/main/java/tech/frits/coffeeorders/MainActivity.java
@@ -1,19 +1,27 @@
 package tech.frits.coffeeorders;
 
+import android.content.Intent;
 import android.icu.text.NumberFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
+
+import static tech.frits.coffeeorders.R.id.name_input;
+import static tech.frits.coffeeorders.R.id.summary_text_view;
 
 public class MainActivity extends AppCompatActivity {
 
     int quantity = 0;
     double price_per_item = 1.99;
     int last_msg_index = 0;
+    String name;
+    String toPay;
+    String summary;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,7 +69,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void displayPrice(int amount) {
-        String toPay = NumberFormat.getCurrencyInstance().format(amount * price_per_item);
+        this.SetName();
+
+        if (name.equals("") || name == null) {
+            Toast.makeText(getApplicationContext(), "Please input a name to order coffee",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        toPay = NumberFormat.getCurrencyInstance().format(amount * price_per_item);
 
         String[] pleasePayMessages = {
                 "That would be " + toPay + " please",
@@ -87,12 +103,41 @@ public class MainActivity extends AppCompatActivity {
 
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(toPay);
+
+        this.SetSummary();
     }
 
     private void setFriendlyMessage(String message) {
         TextView friendly_message = (TextView) findViewById(R.id.friendly_message_text_view);
 
         friendly_message.setText(message);
+    }
+
+    private void SetName() {
+        EditText txtname = (EditText)findViewById(R.id.editText2);
+        name = txtname.getText().toString();
+
+        txtname.setText("");
+    }
+
+    private void SetSummary() {
+        TextView summary_txt = (TextView) findViewById(R.id.summary_text_view);
+
+        summary = "Order summary:\nName: " + name + "\n" +
+                "Amount: " + quantity + "\n"+
+                "Total: " + toPay;
+        summary_txt.setText(summary);
+
+        this.Share();
+    }
+
+    private void Share() {
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Order summary for your coffee");
+        String shareBody = summary;
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+        startActivity(Intent.createChooser(sharingIntent, "Share via"));
     }
 
 }
